d003910c995c1439046393d02ffd7d50b435f4a7
slandath <jesus_slanda@yahoo.com>
2025-10-02 20:51:02 -0400

    added json data template.
    Signed-off-by: slandath <jesus_slanda@yahoo.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef } from "react";
+import bracketData from "./2025-tournament.json";
 import { createBracket } from "./lib/lib.mjs";
-import sampleData from "./sample-data.json";
 
 export default function App() {
   const bracketContainerRef = useRef<HTMLDivElement | null>(null);
@@ -10,7 +10,7 @@ export default function App() {
     if (bracketContainerRef.current) {
       // Install bracket into this container
       bracketInstanceRef.current = createBracket(
-        sampleData, // initial_user_data
+        bracketData, // initial_user_data
         bracketContainerRef.current, // wrapper element
         {
           // no custom options passed in for now
