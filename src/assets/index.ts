064403d36b0b8b6ee07f1e1ca61e54d1ac11ca11
slandath <jesus_slanda@yahoo.com>
2025-12-20 23:56:34 -0500

    moved svg favicon to public directory
    Signed-off-by: slandath <jesus_slanda@yahoo.com>

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,4 +1,3 @@
 export { default as ArrowLeftIcon } from "./ArrowLeftIcon.svg";
 export { default as ArrowRightIcon } from "./ArrowRightIcon.svg";
-export { default as Basketball } from "./Basketball.svg";
 export { default as CloseIcon } from "./CloseIcon.svg";
