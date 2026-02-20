c2a133e203fcc01bb7c0bb72731e4af20c30de65
Tom Slanda <jesus_slanda@yahoo.com>
2026-02-20 12:19:33 -0500

    added tanstack and posted template
    Signed-off-by: Tom Slanda <jesus_slanda@yahoo.com>

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,7 +1,9 @@
 import { createApp } from 'vue'
+import { VueQueryPlugin } from '@tanstack/vue-query'
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 app.use(router)
+app.use(VueQueryPlugin)
 app.mount('#app')
